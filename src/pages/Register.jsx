f39e9377bbc6987d2062a9444c6e29ea38944299
Sarath Kumar Dunga <sarathkumardunga09@gmail.com>
2023-07-17 21:44:06 +0530

    added Toast notifications for form Validation. The calling of API when submitted for register page.

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,16 +1,82 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+//import toast and its css for toast notification to be seen (line 4 and 5) 
+import {ToastContainer, toast} from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+//
+import axios from "axios";
+import { registerRoute } from "../utils/APIRoutes";
 
 function Register(){
+  const [values, setValues] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  //the way a toast notification for validation is shown on diaplay
+  const toastOptions = {
+    position: "bottom-right",
+    autoClose: 8000,
+    pauseOnHover: true,
+    draggable: true,
+    theme: "dark",
+  };
+
   //handle submit definition
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    alert("form");
+    if(handleValidation()){
+      //if the validation is true , gonna call the API
+      //JUs to test if the api is being called correctly.
+      console.log("in validation", registerRoute);
+      const {password, confirmPassword, username, email} = values;
+      const {data} = await axios.post(registerRoute, {
+        username,
+        email,
+        password,
+      });
+    }
+  };
+
+  //handle validation
+  const handleValidation = () => {
+    //we will destructure all the values
+    const {password, confirmPassword, username, email} = values;
+
+    if(confirmPassword !== password){
+      toast.error(
+        "password and confirm password should be same.",
+        toastOptions);
+      return false;
+    }
+    else if (username.length < 3) {
+      toast.error(
+        "Username should be greater than 3 characters.",
+        toastOptions
+      );
+      return false;
+    } 
+    else if (password.length < 8) {
+      toast.error(
+        "Password should be equal or greater than 8 characters.",
+        toastOptions
+      );
+      return false;
+    }
+    else if (email === "") {
+      toast.error("Email is required.", toastOptions);
+      return false;
+    }
+    return true;
   };
 
   //handle change definition
-  const handleChange = (event) => {};
+  const handleChange = (event) => {
+    setValues({ ...values, [event.target.name]:event.target.value});
+  };
 
   //Layout of the register form
   return (
@@ -50,6 +116,7 @@ function Register(){
           </span>
         </form>
       </FormContainer>
+      <ToastContainer />
     </>
   );
 }
