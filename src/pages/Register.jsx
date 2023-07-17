aba4075964a26e12203f76f135e54ab0380c1e37
Sarath Kumar Dunga <sarathkumardunga09@gmail.com>
2023-07-17 18:00:39 +0530

    The interface of Register Page.

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,127 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
-function Register() {
+function Register(){
+  //handle submit definition
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert("form");
+  };
+
+  //handle change definition
+  const handleChange = (event) => {};
+
+  //Layout of the register form
   return (
-    <div>Register</div>
-  )
+    <>    
+      <FormContainer>
+        <form action="" onSubmit={(event) => handleSubmit(event)}>
+          <div className="brand">
+            <h1>Real Chat</h1>
+          </div>
+          <input
+            type="text"
+            placeholder="Username"
+            name="username"
+            onChange={(e) => handleChange(e)}
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            name="email"
+            onChange={(e) => handleChange(e)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            onChange={(e) => handleChange(e)}
+          />
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            name="confirmPassword"
+            onChange={(e) => handleChange(e)}
+          />
+          <button type="submit">Create User</button>
+          <span>
+            Already have an account ? <Link to="/login">Login.</Link>
+          </span>
+        </form>
+      </FormContainer>
+    </>
+  );
 }
 
-export default Register
+//CSS of the register page 
+const FormContainer = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1rem;
+  align-items: center;
+  background-image: url("dot.png");
+  background-color: #4E91C9;
+  .brand {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    justify-content: center;
+  
+    h1 {
+      color: white;
+      text-transform: uppercase;
+    }
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    background-color: #00000079;
+    border-radius: 2rem;
+    padding: 3rem 5rem;
+  }
+  input {
+    background-color: transparent;
+    padding: 1rem;
+    border: 0.1rem solid #4e0eff;
+    border-radius: 0.4rem;
+    color: white;
+    width: 100%;
+    font-size: 1rem;
+    &:focus {
+      border: 0.1rem solid #ffffff;
+      outline: none;
+    }
+  }
+  button {
+    background-color: #4e0eff;
+    color: white;
+    padding: 1rem 2rem;
+    border: none;
+    font-weight: bold;
+    cursor: pointer;
+    border-radius: 0.4rem;
+    font-size: 1rem;
+    text-transform: uppercase;
+    &:hover {
+      background-color: rgb(113, 226, 237);
+      color: rgb(227, 235, 242);
+    }
+  }
+  span {
+    color: white;
+    text-transform: uppercase;
+    a {
+      color: rgb(71, 249, 12);
+      text-decoration: none;
+      font-weight: bold;
+    }
+  }
+`;
+
+export default Register;
