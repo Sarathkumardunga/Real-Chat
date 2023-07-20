ef3bf502607f77a531b56d3e2e4e1a18516f31f7
Sarath Kumar Dunga <sarathkumardunga09@gmail.com>
2023-07-20 15:53:16 +0530

    Finally worked on real time messaging using SOCKET.io

--- src/components/ChatContainer.jsx
+++ src/components/ChatContainer.jsx
@@ -1,22 +1,25 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import styled from "styled-components";
 import Logout from "../components/Logout";
 import ChatInput from "../components/ChatInput";
-//import Messages from "../components/Messages";
 import axios from "axios";
 import { getAllMessagesRoute,sendMessageRoute } from '../utils/APIRoutes';
+import{ v4 as uuidv4 } from "uuid";
 
-
-export default function ChatContainer({ currentChat, currentUser }) {
+export default function ChatContainer({ currentChat, currentUser, socket }) {
   const [messages, setMessages] = useState([]);
+  const [arrivalMessage, setArrivalMessage] = useState(null);
+  const scrollRef = useRef();
 
   useEffect(() => {
     async function fetchData(){
-      const response = await axios.post(getAllMessagesRoute, {
-        from: currentUser._id,
-        to: currentChat._id,
-      });
-      setMessages(response.data);
+      if(currentChat){
+        const response = await axios.post(getAllMessagesRoute, {
+          from: currentUser._id,
+          to: currentChat._id,
+        });
+        setMessages(response.data);
+      }
     }
     fetchData();
   }, [currentChat]);
@@ -27,7 +30,32 @@ export default function ChatContainer({ currentChat, currentUser }) {
       to: currentChat._id,
       message:msg,
     });
+    socket.current.emit("send-msg",{
+      to: currentChat._id,
+      from: currentUser._id,
+      message: msg,
+    });
+
+    const msgs = [...messages];
+    msgs.push({fromSelf:true, message: msg});
+    setMessages(msgs);
   };
+  useEffect(() => {
+    if(socket.current){
+      socket.current.on("msg-receive",(msg)=>{
+        setArrivalMessage({fromSelf: false, message: msg})
+      })
+    }
+  },[]);
+
+  useEffect(()=>{
+    arrivalMessage && setMessages((prev) => [...prev, arrivalMessage]);
+  },[arrivalMessage]);
+
+  useEffect(() => {
+    scrollRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
+  
   return (
     <>
     {
@@ -50,7 +78,7 @@ export default function ChatContainer({ currentChat, currentUser }) {
           <div className="chat-messages">
             {messages.map((message) => {
               return (
-                <div>
+                <div ref={scrollRef} key = {uuidv4()} >
                   <div
                     className={`message ${
                       message.fromSelf ? "sended" : "received"
--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,14 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import styled from "styled-components";
 import axios from "axios";
-
 import { useNavigate } from "react-router-dom";
-import { allUsersRoute } from "../utils/APIRoutes";
+import { allUsersRoute,host } from "../utils/APIRoutes";
 import Contacts from "../components/Contacts";
 import Welcome from "../components/Welcome";
 import ChatContainer from "../components/ChatContainer";
+import { io } from "socket.io-client";
 
 function Chat() {
+  const socket = useRef();
   const navigate = useNavigate();
   const [contacts, setContacts] = useState([]);
   const [currentUser, setCurrentUser] = useState(undefined);
@@ -27,6 +28,13 @@ function Chat() {
     }
     fetchData();
   }, []);
+
+  useEffect(() => {
+    if (currentUser) {
+      socket.current = io(host);
+      socket.current.emit("add-user", currentUser._id);
+    }
+  }, [currentUser]);
 
   useEffect(() => {
     async function fetchData(){
@@ -59,7 +67,10 @@ function Chat() {
         <Welcome 
           currentUser={currentUser}/>
         ) : (
-          <ChatContainer currentChat={currentChat} currentUser={currentUser} />
+          <ChatContainer currentChat={currentChat}
+           currentUser={currentUser}
+            socket={socket}
+          />
         )} 
     </div>
   </Container>
